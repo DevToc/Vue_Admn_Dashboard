57dff2b597e1da99873da4d99f7cd2718beb154e
VasylHryp <maksymkomen0130@gmail.com>
2023-02-15 06:53:15 +0900

    home page button group finish

--- src/views/finance/Home.vue
+++ src/views/finance/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <CForm class="row g-2 justify-content-end mb-4">
+      <div class="col-auto">
+        <CButton color="primary">Export</CButton>
+      </div>
+      <div class="col-auto">
+        <CButton color="primary">Filter</CButton>
+      </div>
+      <div class="col-auto">
+        <WBirthday></WBirthday>
+      </div>
+    </CForm>
     <WidgetsStatsTypeA />
     <WidgetsStatsTypeB />
     <WidgetsStatsTypeC />
@@ -7,14 +18,8 @@
 </template>
 
 <script setup>
-// import avatar1 from '@/assets/images/avatars/1.jpg'
-// import avatar2 from '@/assets/images/avatars/2.jpg'
-// import avatar3 from '@/assets/images/avatars/3.jpg'
-// import avatar4 from '@/assets/images/avatars/4.jpg'
-// import avatar5 from '@/assets/images/avatars/5.jpg'
-// import avatar6 from '@/assets/images/avatars/6.jpg'
 import WidgetsStatsTypeA from '../widgets/WidgetsStatsTypeA.vue'
 import WidgetsStatsTypeB from '../widgets/WidgetsStatsTypeB.vue'
 import WidgetsStatsTypeC from '../widgets/WidgetsStatsTypeC.vue'
-// import WidgetsStatsD from '../widgets/WidgetsStatsTypeD.vue'
+import WBirthday from '../../components/WBirthday.vue'
 </script>
